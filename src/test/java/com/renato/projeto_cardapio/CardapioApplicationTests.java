58c7eb55ffc3d3255de554825694821565fdd068
Renato A. Silva <97402553+coldrenatinho@users.noreply.github.com>
2024-07-08 11:47:08 -0400

    fist commit

---
+++ src/test/java/com/renato/projeto_cardapio/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renato.projeto_cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
